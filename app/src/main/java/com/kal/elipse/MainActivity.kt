732ea60b912d13cf56ccb74a2d4f68b96fa57c30
Avinash Patel <avinash.patel@azentio.com>
2025-07-27 13:11:19 +0530

    Basic Structure

--- app/src/main/java/com/kal/elipse/MainActivity.kt
+++ app/src/main/java/com/kal/elipse/MainActivity.kt
@@ -4,44 +4,136 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kal.elipse.ui.theme.ElipseTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ElipseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text("Elipse")
+                            },
+                            navigationIcon = {
+                                IconButton(
+                                    onClick = {
+
+                                    }
+                                ) {
+                                    Icon(imageVector = Icons.Default.Menu, contentDescription = null)
+                                }
+                            }
+                            , actions = {
+                                IconButton(
+                                    onClick = {}
+                                ) {
+                                    Icon(imageVector = Icons.Default.Search, contentDescription = null)
+
+                                }
+                            }
+                        )
+                    },
+                    bottomBar = {
+                        NavigationBar {
+                            NavigationBarItem(
+                                icon = {
+                                    Icon(
+                                        imageVector = Icons.Default.Home,
+                                        contentDescription = null
+                                    )
+                                },
+                                selected = true,
+                                onClick = {
+
+                                }
+                            )
+                            NavigationBarItem(
+                                icon = {
+                                    Icon(
+                                        imageVector = Icons.Default.Call,
+                                        contentDescription = null
+                                    )
+                                },
+                                selected = true,
+                                onClick = {
+
+                                }
+                            )
+                            NavigationBarItem(
+                                icon = {
+                                    Icon(
+                                        imageVector = Icons.Default.AccountCircle,
+                                        contentDescription = null
+                                    )
+                                },
+                                selected = true,
+                                onClick = {
+
+                                }
+                            )
+                        }
+                    },
+                    floatingActionButton = {
+                        FloatingActionButton(
+                            onClick = {
+
+                            }
+                        ) {
+                            Icon(imageVector = Icons.Default.Add, contentDescription = null)
+                        }
+                    }
+
+                ) { innerPadding ->
+                    Column (modifier = Modifier.padding(innerPadding)){
+                        Card(modifier = Modifier.fillMaxWidth().padding(10.dp)){
+                          Column (verticalArrangement = Arrangement.spacedBy(10.dp), modifier =Modifier.padding(16.dp)){
+                              Text(text = "Hello")
+                              Text(text = "Hello")
+                              Text(text = "Hello")
+                          }
+
+
+                        }
+
+                    }
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ElipseTheme {
-        Greeting("Android")
-    }
-}
